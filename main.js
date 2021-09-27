1db18aee1932ccd8a1d5d189027e43fa019ecfff
Eduardo <eduardodiaz250196@gmail.com>
2021-09-27 19:18:26 -0300

    animation

--- main.js
+++ main.js
@@ -4,11 +4,24 @@
 
 const phoneScreen = document.querySelectorAll(".phone_screen");
 const projectsBtn = document.querySelector("#projects_btn");
-const scrollY = window.pageYOffset;
+const project1 = document.querySelector("#project1"); 
+const projectImg1 = document.querySelector(".project_imgs1"); 
+const project2 = document.querySelector("#project2");
+const projectImg2 = document.querySelector(".project_imgs2"); 
+const project3 = document.querySelector("#project3"); 
+const project4 = document.querySelector("#project4"); 
+const project5 = document.querySelector("#project5");
+const project6 = document.querySelector("#project6");
+const form = document.querySelector(".form_section");
 
 
-projectsBtn.addEventListener("click", phoneScreen.forEach(
-    screen => screen.classList.toggle('screenOn'))) ;
+
+projectsBtn.addEventListener("click", function () {
+    document.querySelectorAll('.project').forEach(project => project.style.display = 'grid');
+    form.style.display = "flex";
+});
+
+
 
 
     
